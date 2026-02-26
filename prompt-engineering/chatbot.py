b256ebab49df87e6e3f1e87bbddd0a88842c49e7
srinijammula <srinijammula5@gmail.com>
2026-02-26 16:56:35 -0500

    chatbot with chat history

--- prompt-engineering/chatbot.py
+++ prompt-engineering/chatbot.py
@@ -3,10 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 model = ChatOpenAI()
+chat_history = []
 
 while True:
     user_input=input('You :')
+    chat_history.append(user_input)
     if user_input=="exit":
         break
-    response=model.invoke(user_input)
+    response=model.invoke(chat_history)
+    chat_history.append(response.content)
     print('Bot :',response.content)
