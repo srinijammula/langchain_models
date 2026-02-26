c4b48648def96444b92d8114b6a0fe348bbadba6
srinijammula <srinijammula5@gmail.com>
2026-02-26 17:15:46 -0500

    langchain messages

--- prompt-engineering/chatbot.py
+++ prompt-engineering/chatbot.py
@@ -1,15 +1,18 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 model = ChatOpenAI()
-chat_history = []
+chat_history = [
+    SystemMessage(content="You are a helpful assistant that provides concise and accurateanswers to user queries."),
+]
 
 while True:
     user_input=input('You :')
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
     if user_input=="exit":
         break
     response=model.invoke(chat_history)
-    chat_history.append(response.content)
+    chat_history.append(AIMessage(content=response.content))
     print('Bot :',response.content)
